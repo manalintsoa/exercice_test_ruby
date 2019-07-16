eb3e033b385da299d2fabf46195c43c13aed94d9
manalintsoa <manalintsoa.1er@gmail.com>
2019-07-16 11:43:10 +0300

    mon test

---
+++ lib/03_basics.rb
@@ -0,0 +1,36 @@
+def who_is_bigger(p1,p2,p3)
+
+  if (p1==nil || p2==nil || p3==nil)
+
+	puts "nil detected"
+
+	return "nil detected"
+ end
+
+
+	hashage={'a'=>p1,'b'=>p2,'c'=>p3}
+
+	valeur=hashage.values.max
+	puts " #{hashage.key(valeur)} is bigger"
+	return " #{hashage.key(valeur)} is bigger"
+end
+
+def reverse_upcase_noLTA(mot)
+
+	phrase = mot.upcase.reverse.tr("LTA","");
+	puts phrase
+	return phrase
+end
+def  array_42(array)
+	return array.include?(42)
+end
+
+def magic_array(x)
+ x.flatten.map{|a| 2 * a}.delete_if{|a| a%3 == 0}.uniq.sort
+end
+
+who_is_bigger(84, 42, nil)
+who_is_bigger(nil, 42, 21)
+who_is_bigger(84, 42, 21)
+who_is_bigger(42, 84, 21)
+who_is_bigger(42, 21, 84)
